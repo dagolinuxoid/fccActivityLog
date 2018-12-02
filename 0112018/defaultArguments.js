b7ae44aa9c3077d5c992e68c9dc856de375f4fb1
dagolinuxoid <daniel1kdago@gmail.com>
2018-12-02 10:49:30 +0300

    defaultArguments version 0.2

--- 0112018/defaultArguments.js
+++ 0112018/defaultArguments.js
@@ -1,15 +1,25 @@
 // https://www.codewars.com/kata/default-arguments/train/javascript
 
-// my initial solution which doesn't work | version 0
+// improved solution which handles state | version 0.2
 
+let box;
 function defaultArguments(fn, o) {
   let list = fn.toString().match(/\((.*?)\)/);
+
   if (list === null) return fn;
-  let defs = list[list.length-1].split(',').map(v=>v.trim());
+  let defs;
+
+  if (list[list.length-1].includes('...')) {
+    defs = box;
+  }
+  else {
+    defs = list[list.length-1].split(',').map(v=>v.trim());
+    box = defs;
+  }
 
   defs = defs.map(v=>o[v]===undefined ? v : o[v]);
 
-  return function (...args){
+  return function hack(...args){
     defs = defs.map((v,i)=>args[i]===undefined ? v : args[i]);
     return fn(...defs);
   }
